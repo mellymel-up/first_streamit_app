c81671455d9a7066107cc90d532fa3dc19eca9ee
mellymel-up <148480339+mellymel-up@users.noreply.github.com>
2023-10-19 14:09:58 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit 
 streamlit.title('My Parents New Healthy Diner')
-Breakfast Menu Omega 3 & Blueberry oatmeal Kale, Spinach & Rocket Smoothie Hard-Boiled Free-Range Egg 
+Breakfast Menu 
+Omega 3 & Blueberry oatmeal 
+Kale, Spinach & Rocket Smoothie 
+Hard-Boiled Free-Range Egg 
